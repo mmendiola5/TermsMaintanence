699b66324217a5c992267cea09dab1b4253ac65b
mmendiola5 <132841622+mmendiola5@users.noreply.github.com>
2023-05-28 13:35:44 -0700

    Add files via upload

---
+++ TermsMaintenance/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// Michael Mendiola 10/9/22
+namespace TermsMaintenance
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void termsBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.termsBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.payablesDataSet);
+
+        }
+
+        private void termsBindingNavigatorSaveItem_Click_1(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.termsBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.payablesDataSet);
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'payablesDataSet.Terms' table. You can move, or remove it, as needed.
+            this.termsTableAdapter.Fill(this.payablesDataSet.Terms);
+
+        }
+
+        private void termsDataGridView_DataError(object sender, DataGridViewDataErrorEventArgs e)
+        {
+            int row = e.RowIndex + 1;
+            string errorMessage = "A data error occured.\n" +
+                "Row: " + row + "\n" +
+                "Error: " + e.Exception.Message;
+            MessageBox.Show(errorMessage, "Data Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void termsDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
